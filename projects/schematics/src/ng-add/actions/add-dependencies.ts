c958d82cbcb03f95cd858571c12ccfef8172bdbd
damienbod <damien_bod@hotmail.com>
2026-08-15 10:30:40 +0200

    22.0.0

--- projects/schematics/src/ng-add/actions/add-dependencies.ts
+++ projects/schematics/src/ng-add/actions/add-dependencies.ts
@@ -5,7 +5,7 @@ import { NgAddOptions } from '../models/ng-add-options';
 const dependenciesToAdd: any[] = [
   {
     name: 'angular-auth-oidc-client',
-    version: '21.0.3',
+    version: '22.0.0',
   },
 ];
 
